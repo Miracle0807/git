b5eb89c700992c733d821971fef226908c0a3810
gaoze <1497369838@qq.com>
2020-04-30 16:47:11 +0800

    新建一个User对象

---
+++ git_test/sources/com/chd/pojo/User.java
@@ -0,0 +1,57 @@
+package com.chd.pojo;
+
+import java.util.Date;
+
+/**
+ * @Author: GaoZe
+ * @Description:
+ * @Date: Create in 16:45 2020/4/30
+ */
+public class User {
+    private String name;
+    private Integer id;
+    private Integer age;
+    private Date birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
